4544fdc6bcf1f473cf03f8c0c2883f40b082dcde
LongTran <longtranduchoang@gmail.com>
2024-09-15 11:00:18 +0700

    Added interface for vcan0 and vcan1 and link them together

--- vcan.sh
+++ vcan.sh
@@ -7,3 +7,14 @@ modprobe vcan
 ip link add dev vcan0 type vcan
 # Bring the virtual CAN interface online.
 ip link set up vcan0
+
+# Create the virtual CAN interface.
+ip link add dev vcan1 type vcan
+# Bring the virtual CAN interface online.
+ip link set up vcan1
+
+sudo cangw -A -s vcan0 -d vcan1 -e
+
+sudo systemctl start systemd-networkd
+
+sudo systemctl enable systemd-networkd
